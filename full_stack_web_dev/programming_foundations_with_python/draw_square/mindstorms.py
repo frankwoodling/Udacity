8a35ee8a854a5d889413391ffd79ed7f0b46bf20
Frank Woodling <fwoodling@gmail.com>
2016-10-16 21:37:16 -0400

    draw to screen function

--- full_stack_web_dev/programming_foundations_with_python/draw_square/mindstorms.py
+++ full_stack_web_dev/programming_foundations_with_python/draw_square/mindstorms.py
@@ -1,23 +1,25 @@
 import turtle
 
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
 
     brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
 
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
-
-    brad.forward(100)
-    brad.right(90)
+    for i in range(1, 37):
+        draw_square(brad)
+        brad.right(10)
 
     window.exitonclick()
 
-draw_square()
+draw_art()
